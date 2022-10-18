28ff7ce1c522b0995431f7bd191269c7c0bef4f1
Eduardo Díaz <ediaz@dcc8090.cl>
2022-10-18 21:03:01 +0000

    agrega test para triangulo pitagorico

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -8,14 +8,13 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    @Test
-    public void probarTriangulo()
-    {
-        assertEquals("error no es un triangulo" , App.triangulo(0, 0,0));
-        assertEquals("es un triangulo equilatero", App.triangulo(1, 1, 1));
-        assertEquals("es un triangulo isoceles", App.triangulo(2, 2, 3));
-        assertEquals("es un triangolo escaleno", App.triangulo(3, 4, 6));
-    }
+public class AppTest {
+  @Test
+  public void probarTriangulo() {
+    assertEquals("error no es un triangulo", App.triangulo(0, 0, 0));
+    assertEquals("es un triangulo equilatero", App.triangulo(1, 1, 1));
+    assertEquals("es un triangulo isoceles", App.triangulo(2, 2, 3));
+    assertEquals("es un triangolo escaleno", App.triangulo(3, 4, 6));
+    assertEquals("es un triangolo pitagorico", App.triangulo(3, 4, 5));
+  }
 }
